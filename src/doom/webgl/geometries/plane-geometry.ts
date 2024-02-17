933ac1663da212dc748ce12ca07e3699cbc7b4a7
Thomas Chandelle <thomas.chandelle@gmail.com>
2024-02-17 11:39:04 +0100

    Adapt Plane

---
+++ src/doom/webgl/geometries/plane-geometry.ts
@@ -0,0 +1,96 @@
+import { Matrix4, ShapeGeometry, ShapePath } from 'three';
+import { FRACBITS } from '../../misc/fixed';
+import { Line } from '../../rendering/defs/line';
+import { Sector } from '../../rendering/defs/sector';
+import { Vertex } from '../../rendering/data/vertex';
+
+const rotate1 = new Matrix4()
+rotate1.set(
+  1, 0, 0, 0,
+  0, 0, 1, 0,
+  0, -1, 0, 0,
+  0, 0, 0, 1,
+)
+const rotate2 = new Matrix4()
+rotate2.set(
+  0, 0, -1, 0,
+  0, 1, 0, 0,
+  1, 0, 0, 0,
+  0, 0, 0, 1,
+)
+
+export class PlaneGeometry extends ShapeGeometry {
+  constructor(
+    sector: Sector,
+    lines: readonly Line[],
+  ) {
+    const shapePath = new ShapePath()
+
+    const clockwise = lines.filter(({ frontSector }) => frontSector === sector)
+      .map(({ v1, v2 }) => [ v1, v2 ] as [Vertex, Vertex])
+    const counterClockWise = lines.filter(({ backSector }) => backSector === sector)
+      .map(({ v1, v2 }) => [ v2, v1 ] as [Vertex, Vertex])
+    const vertices = [ ...clockwise, ...counterClockWise ]
+
+    let firstVertex = vertices[0]
+    let lastVertex: [Vertex, Vertex] | null = null
+
+    let candidates: [Vertex, Vertex][]
+
+    while (vertices.length > 0) {
+      if (lastVertex === null) {
+        firstVertex = lastVertex = vertices[0]
+        vertices.shift()
+
+        shapePath.moveTo(lastVertex[0].x >> FRACBITS, lastVertex[0].y >> FRACBITS)
+      }
+
+      candidates = vertices.filter(([ v1 ]) => lastVertex &&
+        v1.x === lastVertex[1].x && v1.y === lastVertex[1].y)
+
+      if (candidates.length === 0) {
+        // Before giving up and starting a new path, find closest point
+        const lastPoint = lastVertex[1]
+        const closest = [ firstVertex, ...vertices ]
+          .sort(([ a ], [ b ]) => {
+            // Does not bother with sqrt
+            const distA = Math.pow(a.x - lastPoint.x, 2) +
+              Math.pow(a.y - lastPoint.y, 2)
+            const distB = Math.pow(b.x - lastPoint.x, 2) +
+              Math.pow(b.y - lastPoint.y, 2)
+
+            return distA - distB
+          })
+
+        if (closest[0] === firstVertex) {
+          lastVertex = null
+        } else {
+          candidates = closest
+        }
+      }
+
+      if (candidates.length === 0) {
+        lastVertex = null
+      } else {
+        lastVertex = candidates[0]
+        shapePath.lineTo(lastVertex[0].x >> FRACBITS, lastVertex[0].y >> FRACBITS)
+        vertices.splice(vertices.indexOf(lastVertex), 1)
+      }
+    }
+
+    super(shapePath.toShapes(false))
+
+    // Use matrix for more precise rotation
+    // this.rotateX(-Math.PI / 2)
+    this.applyMatrix4(rotate1)
+
+    // this.rotateY(-Math.PI / 2)
+    this.applyMatrix4(rotate2)
+
+    this.faceVertexUvs[0].forEach(uvs => {
+      uvs.forEach(uv => {
+        uv.divideScalar(64)
+      })
+    })
+  }
+}
--- src/doom/webgl/plane.ts
+++ src/doom/webgl/plane.ts
@@ -1,45 +1,27 @@
 import {
   BackSide,
   FrontSide,
-  Matrix4,
   Mesh,
   Object3D,
-  ShapeGeometry,
-  ShapePath,
   Side,
 } from 'three'
 import { FRACBITS } from '../misc/fixed'
 import { Plane as LegacyPlane } from '../rendering/plane'
 import { Line } from '../rendering/defs/line'
 import { MeshBasicPaletteMaterial } from './materials/mesh-basic-palette-material'
+import { PlaneGeometry } from './geometries/plane-geometry'
 import { Rendering } from './rendering'
 import { Sector } from '../rendering/defs/sector'
 import { Seg } from '../rendering/segs/seg'
 import { Textures } from './textures'
-import { Vertex } from '../rendering/data/vertex'
 
-type FloorOrCeilingMesh = Mesh<ShapeGeometry, MeshBasicPaletteMaterial>
+type FloorOrCeilingMesh = Mesh<PlaneGeometry, MeshBasicPaletteMaterial>
 
 interface FloorAndCeiling {
   sector: Sector
   floor: FloorOrCeilingMesh
   ceiling: FloorOrCeilingMesh
 }
-
-const rotate1 = new Matrix4()
-rotate1.set(
-  1, 0, 0, 0,
-  0, 0, 1, 0,
-  0, -1, 0, 0,
-  0, 0, 0, 1,
-)
-const rotate2 = new Matrix4()
-rotate2.set(
-  0, 0, -1, 0,
-  0, 1, 0, 0,
-  1, 0, 0, 0,
-  0, 0, 0, 1,
-)
 
 export class Plane extends LegacyPlane {
 
@@ -124,72 +106,8 @@ export class Plane extends LegacyPlane {
   }
 
   private createMesh(sector: Sector, lines: Line[], side: Side): FloorOrCeilingMesh {
-    const shapePath = new ShapePath()
-
-    const clock = lines.filter(({ frontSector }) => frontSector === sector)
-      .map(({ v1, v2 }) => [ v1, v2 ] as [Vertex, Vertex])
-    const antiClock = lines.filter(({ backSector }) => backSector === sector)
-      .map(({ v1, v2 }) => [ v2, v1 ] as [Vertex, Vertex])
-    const vertexes = [ ...clock, ...antiClock ]
-
-    let firstVertexes = vertexes[0]
-    let lastVertexes: [Vertex, Vertex] | null = null
-
-    let candidates: [Vertex, Vertex][]
-
-    while (vertexes.length > 0) {
-      if (lastVertexes === null) {
-        firstVertexes = lastVertexes = vertexes[0]
-        vertexes.shift()
-
-        shapePath.moveTo(lastVertexes[0].x >> FRACBITS, lastVertexes[0].y >> FRACBITS)
-      }
-
-      candidates = vertexes.filter(([ v1 ]) => lastVertexes &&
-        v1.x === lastVertexes[1].x && v1.y === lastVertexes[1].y)
-
-      if (candidates.length === 0) {
-        // Before giving up and starting a new path, find closest point
-        const lastPoint = lastVertexes[1]
-        const closest = [ firstVertexes, ...vertexes ]
-          .sort(([ a ], [ b ]) => {
-            // Does not bother with sqrt
-            const distA = Math.pow(a.x - lastPoint.x, 2) +
-              Math.pow(a.y - lastPoint.y, 2)
-            const distB = Math.pow(b.x - lastPoint.x, 2) +
-              Math.pow(b.y - lastPoint.y, 2)
-
-            return distA - distB
-          })
-
-        if (closest[0] === firstVertexes) {
-          lastVertexes = null
-        } else {
-          candidates = closest
-        }
-      }
-
-      if (candidates.length === 0) {
-        lastVertexes = null
-      } else {
-        lastVertexes = candidates[0]
-        shapePath.lineTo(lastVertexes[0].x >> FRACBITS, lastVertexes[0].y >> FRACBITS)
-        vertexes.splice(vertexes.indexOf(lastVertexes), 1)
-      }
-    }
-
-    const geometry = new ShapeGeometry(shapePath.toShapes(false))
-    geometry.applyMatrix4(rotate1)
-    geometry.applyMatrix4(rotate2)
-
-    geometry.faceVertexUvs[0].forEach(uvs => {
-      uvs.forEach(uv => {
-        uv.divideScalar(64)
-      })
-    })
-
     const mesh = new Mesh(
-      geometry,
+      new PlaneGeometry(sector, lines),
       new MeshBasicPaletteMaterial({ side }),
     )
     mesh.visible = false
@@ -199,12 +117,7 @@ export class Plane extends LegacyPlane {
 
   private updateGeometryHeight(mesh: FloorOrCeilingMesh, y: number): void {
     y >>= FRACBITS
-    if (mesh.geometry.vertices[0].y !== y) {
-      mesh.geometry.vertices.forEach(v => v.y = y)
-
-      mesh.geometry.verticesNeedUpdate = true
-      mesh.geometry.computeBoundingSphere()
-    }
+    mesh.position.y = y
   }
 
   private updateTextureMap(mesh: FloorOrCeilingMesh, flat: number): void {
