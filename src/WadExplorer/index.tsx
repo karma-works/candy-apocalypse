08f390a74cf56a7d8d2f57eb5feb07698c3a5aac
Thomas Chandelle <thomas.chandelle@gmail.com>
2024-03-13 20:22:20 +0100

    Wad Explorer: Configurable palette and colormap

--- src/WadExplorer/index.tsx
+++ src/WadExplorer/index.tsx
@@ -8,53 +8,65 @@ import MObjSelector from './MObjSelector';
 import { MObjType } from '../doom/doom/info/mobj-type';
 import { Perf } from 'r3f-perf';
 import { WadProvider } from './WadContext';
+import { usePaletteControls } from './usePaletteControls';
 import { useSearchParams } from 'react-router-dom';
 
-export default function WadExplorer() {
+function WadExplorer() {
   const [ searchParams ] = useSearchParams();
-  const fileNames = useMemo(() => {
-    return [ searchParams.get('iwad') || 'doom1.wad' ]
-  }, [ searchParams ])
   const debug = searchParams.get('debug') !== null
+
+  usePaletteControls()
 
   const [ levelName, setLevelName ] = useState<string | null>(null)
   const [ mObjType, setMObjType ] = useState<MObjType | null>(null)
 
   return (
-    <WadProvider fileNames={fileNames}>
-      <div className="WadExplorer">
-        <div className="WadExplorerToolbar">
-          <LevelSelector
-            levelName={levelName}
-            onChangeLevelName={l => {
-              setMObjType(null)
-              setLevelName(l)
-            }}
-          />
-
-          <MObjSelector
-            mObjType={mObjType}
-            onChangeMObjType={n => {
-              setLevelName(null)
-              setMObjType(n)
-            }}
-          />
-        </div>
-        <Canvas
-          camera={{
-            position: [ -512, 512, -512 ],
-            far: 4000,
+    <div className="WadExplorer">
+      <div className="WadExplorerToolbar">
+        <LevelSelector
+          levelName={levelName}
+          onChangeLevelName={l => {
+            setMObjType(null)
+            setLevelName(l)
           }}
-        >
-          { debug && <Perf position="top-left" /> }
+        />
 
-          { levelName &&
-            <Level levelName={levelName} /> }
-
-          { mObjType !== null &&
-            <MObj mObjType={mObjType} />}
-        </Canvas>
+        <MObjSelector
+          mObjType={mObjType}
+          onChangeMObjType={n => {
+            setLevelName(null)
+            setMObjType(n)
+          }}
+        />
       </div>
+      <Canvas
+        camera={{
+          position: [ -512, 512, -512 ],
+          far: 4000,
+        }}
+      >
+        { debug && <Perf position="top-left" /> }
+
+        { levelName &&
+          <Level levelName={levelName} /> }
+
+        { mObjType !== null &&
+          <MObj mObjType={mObjType} />}
+      </Canvas>
+    </div>
+  )
+}
+
+
+export default function WadExplorerRoot() {
+  const [ searchParams ] = useSearchParams();
+  const fileNames = useMemo(() => {
+    return [ searchParams.get('iwad') || 'doom1.wad' ]
+  }, [ searchParams ])
+
+  return (
+    <WadProvider fileNames={fileNames}>
+      <WadExplorer />
     </WadProvider>
   )
 }
