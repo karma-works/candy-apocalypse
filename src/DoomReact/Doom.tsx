87a37232cd925f92961174940b4c193e889214bb
Thomas Chandelle <thomas.chandelle@gmail.com>
2024-03-21 11:47:18 +0100

    Cleaner Doom react component

--- src/DoomReact/Doom.tsx
+++ src/DoomReact/Doom.tsx
@@ -1,5 +1,5 @@
 import './Doom.css'
-import { CSSProperties, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { CSSProperties, RefObject, useCallback, useEffect, useRef, useState } from 'react';
 import { EvType } from '../doom/doom/event';
 import FocusInfo from './FocusInfo';
 import { Params } from '../doom/doom/params'
@@ -8,136 +8,24 @@ import { ScanCode } from '../doom/interfaces/scancodes';
 import { useAudio as useAudioContext } from '../AudioContext';
 import { useSearchParams } from 'react-router-dom';
 
-function useAudio() {
-  const audioCtx = useAudioContext()
-  const [ searchParams ] = useSearchParams();
-
-  return useMemo(() => {
-    return searchParams.has('sound') ? audioCtx : null
-  }, [ audioCtx, searchParams ])
-}
-
 export default function Doom(props?: Partial<Params>) {
-  const [ running, setRunning ] = useState(false)
-
-  const [ canvasStyle, setCanvasStyle ] = useState<CSSProperties>({})
   const inputRef = useRef<HTMLDivElement>(null)
   const screen2dRef = useRef<HTMLCanvasElement>(null)
   const screen3dRef = useRef<HTMLCanvasElement>(null)
 
-  const doomInstRef = useRef<RawDoom>()
+  const doomInst = useDoomInst(inputRef, screen2dRef, screen3dRef, props)
 
-  const start = useCallback(async() => {
-    try {
-      const input = inputRef.current!
-      const screen2d = screen2dRef.current!
-      const screen3d = screen3dRef.current!
+  const canvasStyle = useCanvasResizer(doomInst)
 
-      const doomInst = new RawDoom({
-        input,
-        screen2d,
-        screen3d,
-        iwad: 'doom1.wad',
-        ...props,
-      })
-      doomInstRef.current = doomInst
-      doomInst.onError = console.error
+  useAudioEnabler(doomInst, inputRef)
 
-      await doomInst.init()
-    } catch (e) {
-      console.error(e)
-    }
-  }, [ props ])
-
-  const stop = useCallback(() => {
-    if (doomInstRef.current) {
-      doomInstRef.current.quit()
-      doomInstRef.current = undefined
-    }
-  }, [])
-
-  const audioCtx = useAudio()
-  const updateSoundCtx = useCallback(() => {
-    if (doomInstRef.current) {
-      doomInstRef.current.iSound.audioCtx = audioCtx
-    }
-  }, [ audioCtx ])
-  useEffect(updateSoundCtx, [ updateSoundCtx ])
-
-  function onResize() {
-    setCanvasStyle({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    })
-    doomInstRef.current?.iVideo.setSize(window.innerWidth, window.innerHeight)
-  }
-
-  const onPointerLockChange = useCallback(() => {
-    const running = document.pointerLockElement === inputRef.current
-    setRunning(running)
-
-    if (running) {
-      inputRef.current?.focus()
-    } else {
-      // Propage missing KeyDown Escape event
-      doomInstRef.current?.postEvent({
-        type: EvType.KeyDown,
-        data1: ScanCode.Escape,
-        data2: 0,
-        data3: 0,
-      })
-      onBlur()
-    }
-  }, [])
-
-  function onFocus() {
-    audioCtx?.resume()
-
-    setTimeout(() => {
-      inputRef.current?.focus()
-      inputRef.current?.requestPointerLock()
-
-      if (navigator.keyboard) {
-        navigator.keyboard.lock()
-      }
-    }, 0)
-    setRunning(true)
-  }
-  function onBlur() {
-    setTimeout(() => {
-      inputRef.current?.blur()
-      if (navigator.keyboard) {
-        navigator.keyboard.unlock()
-      }
-    }, 0)
-    setRunning(false)
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', onResize);
-    document.addEventListener('pointerlockchange', onPointerLockChange);
-    document.addEventListener('pointerlockerror', onBlur);
-    return () => {
-      window.removeEventListener('resize', onResize);
-      document.removeEventListener('pointerlockchange', onPointerLockChange);
-      document.removeEventListener('pointerlockerror', onBlur);
-    }
-  }, [ onPointerLockChange ]);
-
-  useEffect(() => {
-    start();
-    updateSoundCtx()
-    onResize()
-    return stop;
-  }, [ start, updateSoundCtx, stop ])
+  const pointerLocked = usePointerLocker(doomInst, inputRef)
 
   return (
     <>
       <div
         className="screen-root"
         ref={inputRef}
-        onFocus={onFocus}
-        onBlur={onBlur}
       >
         <div className="screen-wrapper">
           <canvas ref={screen3dRef} style={canvasStyle}>
@@ -147,7 +35,150 @@ export default function Doom(props?: Partial<Params>) {
         </div>
       </div>
 
-      { !running && <FocusInfo />}
+      { !pointerLocked && <FocusInfo />}
     </>
   )
+}
+
+function useDoomInst(
+  inputRef: RefObject<HTMLDivElement>,
+  screen2dRef: RefObject<HTMLCanvasElement>,
+  screen3dRef: RefObject<HTMLCanvasElement>,
+  params: Partial<Params> = {},
+) {
+  const [ doom, setDoom ] = useState<RawDoom>()
+
+  useEffect(() => {
+    const input = inputRef.current!
+    const screen2d = screen2dRef.current!
+    const screen3d = screen3dRef.current!
+
+    const doomInst = new RawDoom({
+      input,
+      screen2d,
+      screen3d,
+      iwad: 'doom1.wad',
+      ...params,
+    })
+    setDoom(doomInst)
+    doomInst.onError = console.error
+    try {
+      /* await */ doomInst.init()
+    } catch (e) {
+      console.error(e)
+    }
+
+    return () => doomInst.quit()
+  }, [ params, inputRef, screen2dRef, screen3dRef ])
+
+  return doom
+}
+
+function useCanvasResizer(doomInst: RawDoom | undefined) {
+  const [ canvasStyle, setCanvasStyle ] = useState<CSSProperties>({})
+
+  const onResize = useCallback(() => {
+    setCanvasStyle({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    })
+    doomInst?.iVideo.setSize(window.innerWidth, window.innerHeight)
+  }, [ doomInst ])
+
+  useEffect(() => {
+    onResize()
+
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize)
+  }, [ onResize ]);
+
+  return canvasStyle
+}
+
+function useAudioEnabler(
+  doomInst: RawDoom | undefined,
+  inputRef: RefObject<HTMLDivElement>,
+) {
+  const audioCtx = useAudioContext()
+  const [ searchParams ] = useSearchParams();
+  const hasSound = searchParams.has('sound')
+
+  useEffect(() => {
+    if (!hasSound || !doomInst) {
+      return
+    }
+
+    doomInst.iSound.audioCtx = audioCtx
+
+    const input = inputRef.current!
+    const onFocus = () => audioCtx.resume()
+    input.addEventListener('focus', onFocus)
+    return () => {
+      doomInst.iSound.audioCtx = null
+      input.removeEventListener('focus', onFocus)
+    }
+  }, [ audioCtx, doomInst, hasSound, inputRef ])
+}
+
+function usePointerLocker(
+  doomInst: RawDoom | undefined,
+  inputRef: RefObject<HTMLDivElement>,
+) {
+  const [ pointerLocked, setPointerLocked ] = useState(false)
+
+  const onFocus = useCallback(() => {
+    setTimeout(() => {
+      inputRef.current!.focus()
+      inputRef.current!.requestPointerLock()
+
+      if (navigator.keyboard) {
+        navigator.keyboard.lock()
+      }
+    }, 0)
+    setPointerLocked(true)
+  }, [ inputRef ])
+
+  const onBlur = useCallback(() => {
+    setTimeout(() => {
+      inputRef.current!.blur()
+      if (navigator.keyboard) {
+        navigator.keyboard.unlock()
+      }
+    }, 0)
+    setPointerLocked(false)
+  }, [ inputRef ])
+
+  const onPointerLockChange = useCallback(() => {
+    const running = document.pointerLockElement === inputRef.current
+    setPointerLocked(running)
+
+    if (running) {
+      inputRef.current!.focus()
+    } else {
+      // Propage missing KeyDown Escape event
+      doomInst?.postEvent({
+        type: EvType.KeyDown,
+        data1: ScanCode.Escape,
+        data2: 0,
+        data3: 0,
+      })
+      onBlur()
+    }
+  }, [ doomInst, inputRef, onBlur ])
+
+  useEffect(() => {
+    const input = inputRef.current!
+    input.addEventListener('focus', onFocus)
+    input.addEventListener('blur', onBlur)
+    document.addEventListener('pointerlockchange', onPointerLockChange);
+    document.addEventListener('pointerlockerror', onBlur);
+    return () => {
+      input.removeEventListener('focus', onFocus)
+      input.removeEventListener('blur', onBlur)
+      document.removeEventListener('pointerlockchange', onPointerLockChange);
+      document.removeEventListener('pointerlockerror', onBlur);
+    }
+  }, [ inputRef, onBlur, onFocus, onPointerLockChange ])
+
+  return pointerLocked
 }
