9c4ed5237d9c88889d3d00044a262030e792f035
karma.works <christian@haegele.dev>
2026-03-14 19:05:59 +0100

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import { env } from "node:process";
 import react from "@vitejs/plugin-react";
 
+const base = env.CI_PROJECT_NAME
+  ? `/${env.CI_PROJECT_NAME}/`
+  : env.GITHUB_REPOSITORY
+    ? `/${env.GITHUB_REPOSITORY.split("/")[1]}/`
+    : "";
+
 export default defineConfig({
-  base: env.CI_PROJECT_NAME ? `/${env.CI_PROJECT_NAME}/` : "",
+  base,
   plugins: [react()],
 });
